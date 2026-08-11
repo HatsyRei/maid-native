2887e504d0b8732042b38429a8a0f890768f23d9
HatsyRei <yan.huan.chng@emerson.com>
2026-08-11 16:00:36 +0800

    chore: bump app version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hatsyrei.maidnative"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.3.0"
+        versionCode = 7
+        versionName = "1.3.1"
     }
 
     buildTypes {
