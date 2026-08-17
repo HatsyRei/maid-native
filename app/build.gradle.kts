a3b87512bc53f60a54b3413f140f7e7e94583955
HatsyRei <yan.huan.chng@emerson.com>
2026-08-17 18:43:38 +0800

    chore: bump app version to 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hatsyrei.maidnative"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.4.0"
+        versionCode = 11
+        versionName = "1.4.1"
     }
 
     buildTypes {
