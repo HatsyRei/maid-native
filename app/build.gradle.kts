0105529a0aafff8a92fc4f1905d3d9b88a3336c1
HatsyRei <yan.huan.chng@emerson.com>
2026-09-02 17:23:02 +0800

    Release 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hatsyrei.maidnative"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.5.0"
+        versionCode = 15
+        versionName = "1.5.1"
     }
 
     buildTypes {
